d42ec391bdacc903714be966901ac542be31f5b3
Tom Ritchford <tom@swirly.com>
2023-04-21 21:14:23 +0200

    Fill out fortune

--- fortune.py
+++ fortune.py
@@ -1,7 +1,22 @@
+from functools import cache
+from mastodon import Mastodon
 import subprocess
+import time
+
 
 KWARGS = {'text': True, 'stdout': subprocess.PIPE}
 MAX_LEN = 500
+ACCESS_TOKEN = open('access-token.txt').read().strip()
+BASE_URL = 'https://botsin.space/'
+DELAY = 3600
+
+
+@cache
+def mastodon():
+    return Mastodon(
+        access_token = ACCESS_TOKEN,
+        api_base_url = 'https://botsin.space/'
+    )
 
 
 def get_fortune():
@@ -10,4 +25,14 @@ def get_fortune():
     return f
 
 
-print(get_fortune(), end='')
+def main():
+    while True:
+        f = get_fortune()
+        print(f)
+        mastodon().status_post(f)
+
+        time.sleep(DELAY)
+
+
+if __name__ == '__main__':
+    main()
